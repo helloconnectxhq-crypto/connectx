3931234162bb54468b1b4a8a477c4ec5d474d81a
Rijan Thapa <rijanthapaofficial@gmail.com>
2026-06-27 09:53:03 +0545

    add SEO sitemap robots manifest sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'connectx-v1'
+const CACHE_NAME = 'connectx-v3'
 const ASSETS = [
   '/',
   '/index.html',
@@ -6,19 +6,43 @@ const ASSETS = [
   '/profile.html',
   '/search.html',
   '/messages.html',
+  '/reels.html',
+  '/live.html',
   '/signup.html',
   '/style.css',
-  '/supabase.js'
+  '/supabase.js',
+  '/realtime.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon-192-maskable.png',
+  '/icon-512-maskable.png'
 ]
 
 self.addEventListener('install', e => {
+  self.skipWaiting()
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   )
 })
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
   )
 })
